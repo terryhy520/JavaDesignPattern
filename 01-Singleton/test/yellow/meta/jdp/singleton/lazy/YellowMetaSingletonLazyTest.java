cd83df71639b4b177a4d7a99f11660b96155b142
terryhy520@qq.com <terry@litingdeMacBook-Pro.local>
2022-07-15 23:56:16 +0800

    hungry and lazy singleton test

---
+++ 01-Singleton/test/yellow/meta/jdp/singleton/lazy/YellowMetaSingletonLazyTest.java
@@ -0,0 +1,21 @@
+package yellow.meta.jdp.singleton.lazy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class YellowMetaSingletonLazyTest {
+
+    @Test
+    public void getInstance() {
+        YellowMetaSingletonLazy instance0 = YellowMetaSingletonLazy.getInstance();
+        int memoryAddress = System.identityHashCode(instance0);
+        for (int i = 0; i < 50; i++) {
+            YellowMetaSingletonLazy instance = YellowMetaSingletonLazy.getInstance();
+            assertTrue(instance==instance0);
+            assertEquals(System.identityHashCode(instance), memoryAddress);
+        }
+    }
+}
