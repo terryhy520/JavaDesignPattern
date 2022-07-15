87bd98f1717b01cfd357efb1e1b36dcb45cdd2fc
terry <terry@litingdeMacBook-Pro.local>
2022-07-15 23:09:14 +0800

    singleton hungry and lazy model

---
+++ 01-Singleton/src/yellow/meta/jdp/singleton/hungry/YellowMetaSingletonHungry.java
@@ -0,0 +1,11 @@
+package yellow.meta.jdp.singleton.hungry;
+
+public class YellowMetaSingletonHungry {
+    private static final YellowMetaSingletonHungry instance = new YellowMetaSingletonHungry();
+
+    private YellowMetaSingletonHungry() {}
+
+    public static YellowMetaSingletonHungry getInstance() {
+        return instance;
+    }
+}
---
+++ 01-Singleton/src/yellow/meta/jdp/singleton/lazy/YellowMetaSingletonLazy.java
@@ -0,0 +1,14 @@
+package yellow.meta.jdp.singleton.lazy;
+
+public class YellowMetaSingletonLazy {
+    private static YellowMetaSingletonLazy instance;
+
+    private YellowMetaSingletonLazy() {}
+
+    public static synchronized YellowMetaSingletonLazy getInstance() {
+        if (instance == null) {
+            instance = new YellowMetaSingletonLazy();
+        }
+        return instance;
+    }
+}
